a4fefd0caa2f86c35bfda59dfb1cae6852cd4dec
refaelok <refaeloknin@gmail.com>
2017-04-14 02:37:32 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,7 @@ const gulp = require('gulp');
 const template = require('gulp-template');
 
 gulp.task('createCoreComponent', () =>
-	gulp.src('generator/templates/client/core-component-template',base: {'./'})
-		.pipe(gulp.dest('client/src'))
+	  gulp.src('generator/templates/client/core-component-template', base: {'./'})
+		  .pipe(rename('client/src/components/core/' + process.argv[4]))
+		  .pipe(gulp.dest("./"));
 );
-
-functio getArg (name) {
-  var i = process.argv.indexOf(name);
-  if(i>-1) {
-      return process.argv[i+1];
-  }
-  return null;
-}
