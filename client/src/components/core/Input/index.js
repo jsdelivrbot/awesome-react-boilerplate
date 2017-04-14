f67cc63195ed1c1b68ec59d3bbe012e2c57d1d90
refaelok <refaeloknin@gmail.com>
2017-04-14 13:57:50 +0300

    Done Posts new

--- client/src/components/core/Input/index.js
+++ client/src/components/core/Input/index.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { createField } from 'redux-form-field';
 
-const component = ({ meta: { touched, error, warning }, input, type, label }) => {
+const component = ({ meta: { touched, error, warning, invalid }, input, type, label }) => {
 
     return (
-        <div className="form-group">
+        <div className={`form-group ${touched && invalid ? 'has-danger': ''}`}>
             <label>{label}</label>
             <div>
                 <input {...input} placeholder={label} type={type} className="form-control"/>
-                {error}
+                {touched && error}
             </div>
         </div>
     );
--- client/src/components/core/Textarea/index.js
+++ client/src/components/core/Textarea/index.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { createField } from 'redux-form-field';
 
-const component = ({ meta: { touched, error, warning }, input, label }) => {
+const component = ({ meta: { touched, error, warning, invalid }, input, label }) => {
 
     return (
-        <div className="form-group">
+        <div className={`form-group ${touched && invalid ? 'has-danger': ''}`}>
             <label>{label}</label>
             <div>
                 <textarea {...input} placeholder={label} className="form-control"/>
-                {error}
+                {touched && error}
             </div>
         </div>
     );
