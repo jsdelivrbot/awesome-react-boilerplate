2f82146e2010cbb50983fe65593329db1cb7b6bd
refaelok <refaeloknin@gmail.com>
2017-04-15 01:56:51 +0300

    Navigate on submit

--- client/src/actions/posts/actions_types.js
+++ client/src/actions/posts/actions_types.js
@@ -1,4 +1,6 @@
 
 export const FETCH_POSTS = 'FETCH_POSTS';
-export const CREATE_POST = 'CREATE_POST';
+export const CREATE_POST = 'CREATE_POST'
+export const INITIAL_STATE = 'INITIAL_STATE';
+
 
--- client/src/reducers/posts/reducer_posts.js
+++ client/src/reducers/posts/reducer_posts.js
@@ -1,12 +1,22 @@
-import { FETCH_POSTS } from '../../actions/posts/actions_types';
+import { INITIAL_STATE, FETCH_POSTS, CREATE_POST } from '../../actions/posts/actions_types';
 
-const INITIAL_STATE = { all: [], post: null };
+const INIT_STATE = { all: [], post: null, createPostSuccess: null };
 
-export default function (state = INITIAL_STATE, action) {
+export default function (state = INIT_STATE, action) {
     switch(action.type) {
 
+        case INITIAL_STATE:
+            return { ...state, createPostSuccess: null };
+
         case FETCH_POSTS:
-            return { ...state, all: action.payload.data }
+            return { ...state, all: action.payload.data };
+
+        case CREATE_POST:
+            if(action.payload.request.status == 201)
+                return { ...state, createPostSuccess: true };
+            else
+                return { ...state, createPostSuccess: false };
+
         default:
             return state;
     }
