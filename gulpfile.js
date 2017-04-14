622b40b1c2931ac96c317c94947c3be5e48f9903
refaelok <refaeloknin@gmail.com>
2017-04-14 18:44:50 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,24 @@
 const gulp = require('gulp');
 const template = require('gulp-template');
-const footer = require('gulp-footer');;
+const footer = require('gulp-footer');
 const rename = require("gulp-rename");
+
+
+gulp.task('createComponent', () => {
+
+    let componentName =getArg('name');
+
+    if(!componentName) {
+        console.error("ERR! You are not select mandatory parameter '--name'");
+        return;
+    }
+
+    // add new component
+    gulp.src('./generator/templates/client/component-template')
+        .pipe(rename('client/src/components/' + 'dmb.' + componentName + '.js'))
+        .pipe(gulp.dest("./"));
+
+});
 
 gulp.task('createCoreComponent', () => {
 
@@ -14,15 +31,16 @@ gulp.task('createCoreComponent', () => {
 
     // add new component and folder to core
     gulp.src('./generator/templates/client/core-component-template')
-        .pipe(rename('client/src/components/core/' + '/' + componentName + '/'  + componentName + '.js'))
+        .pipe(rename('client/src/components/core/' + 'cor.' + componentName + '/'  + 'index.js'))
         .pipe(gulp.dest("./"));
 
     // update index.js in core
     gulp.src('./client/src/components/core/index.js')
-        .pipe(footer("export { default as <%= name %> } from './<%= name %>';\n", { name : componentName} ))
+        .pipe(footer("export { default as <%= name %> } from './<%= name %>';\n", { name : 'cor.' + componentName} ))
         .pipe(gulp.dest('./client/src/components/core/'));
 
 });
+
 
 function getArg(name) {
 
