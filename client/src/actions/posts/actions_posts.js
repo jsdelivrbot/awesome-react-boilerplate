48124a308d6fa3fd132d1f8d6ac010966fb3cb42
refaelok <refaeloknin@gmail.com>
2017-04-16 02:52:08 +0300

    Update actions_posts.js

--- client/src/actions/posts/actions_posts.js
+++ client/src/actions/posts/actions_posts.js
@@ -3,9 +3,14 @@ import request from '../../utiles/requests';
 import { REDUXBLOG_ROOT_URL } from '../../api';
 
 const API_KEY = 'refaelok';
+const POSTS_URL = `${REDUXBLOG_ROOT_URL}/posts`;
 
 export function fetchPosts () {
-    const response = request('get', `${REDUXBLOG_ROOT_URL}/posts`, { key: API_KEY });
+    const response = request({
+        method: 'get',
+        url: POSTS_URL,
+        params: {key: API_KEY}
+    });
 
     return {
         type: FETCH_POSTS,
@@ -14,7 +19,12 @@ export function fetchPosts () {
 }
 
 export function createPost(props, sucessHandler, errorHandler) {
-    const response = request('post', `${REDUXBLOG_ROOT_URL}/posts?key=${API_KEY}`, props, sucessHandler, errorHandler);
+    const response = request({
+        method: 'post',
+        url: POSTS_URL,
+        data: props,
+        params: {key: API_KEY}
+    }, sucessHandler, errorHandler);
 
     return {
         type: CREATE_POST,
