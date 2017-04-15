cedc249eef8887feb3a54913f8b6891397fa2843
refaelok <refaeloknin@gmail.com>
2017-04-15 13:05:33 +0300

    Update requests.js

--- client/src/utiles/requests.js
+++ client/src/utiles/requests.js
@@ -1,1 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+function requestCall(method, url, options, successHandler, errorHandler) {
+  // add here your code before any request call, for example loading spinner.
+  
+  switch (method) {
+    case: 'get':
+      get(url, options, successHandler, errorHandler);
+      break;
+     case: 'post':
+      get(url, options, successHandler, errorHandler);
+      break;
+  }
+
+}
+
+function* get(url, params, successHandler, errorHandler) {
+  const request = yield axios.get(url, {params: params});
+  
+  return request;
+}
+
+function* post(url, payload, successHandler, errorHandler) {
+  const request = yield axios.post(url, payload);
+  
+  return request;
+}
+
+
+export defualt requestCall;
