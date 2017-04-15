400520cd15cbd55575ad29993ea76d718f51385a
refaelok <refaeloknin@gmail.com>
2017-04-16 02:21:47 +0300

    Update reducer_posts.js

--- client/src/reducers/posts/reducer_posts.js
+++ client/src/reducers/posts/reducer_posts.js
@@ -1,17 +1,25 @@
+import { fromJS } from 'immutable';
 import { FETCH_POSTS, CREATE_POST } from '../../actions/posts/actions_types';
 
-const INITIAL_STATE = { all: [], post: null };
+const INITIAL_STATE = fromJS({
+    all: [], 
+    post: null
+});
 
 export default function (state = INITIAL_STATE, action) {
     switch(action.type) {
 
         case INITIAL_STATE:
-            return { ...state, createPostSuccess: null };
+            return state.set({
+                all: [], 
+                post: null
+            });
 
         case FETCH_POSTS:
             return { ...state, all: action.payload.data };
 
         default:
             return state;
+            
     }
 }
