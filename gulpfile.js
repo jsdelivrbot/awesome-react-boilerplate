9c1c409423d4183a4e51e5f2f450b9619812fa47
refaelok <refaeloknin@gmail.com>
2017-04-16 03:28:06 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,9 @@ const rename = require("gulp-rename");
 
 
 gulp.task('createComponent', () => {
-
     let componentName =getArg('name');
-
-    if(!componentName) {
-        console.error("ERR! You are not select mandatory parameter value '--name'");
-        return;
-    }
-
-    if(componentName[0] !== componentName[0].toUpperCase()) {
-        console.error("ERR! '--name' value must start with uppercase");
-        return;
-    }
+    
+    if(!validateName(componentName, '--name', true)) return;
 
     // add new component
     createTemplate(
@@ -28,18 +19,9 @@ gulp.task('createComponent', () => {
 });
 
 gulp.task('createCoreComponent', () => {
-
     let componentName =getArg('name');
-
-    if(!componentName) {
-        console.error("ERR! You are not select mandatory parameter value '--name'");
-        return;
-    }
-
-    if(componentName[0] !== componentName[0].toUpperCase()) {
-        console.error("ERR! '--name' value must start with uppercase");
-        return;
-    }
+    
+    if(!validateName(componentName, '--name', true)) return;
 
     // add new component and folder to core
     createTemplate(
@@ -55,7 +37,6 @@ gulp.task('createCoreComponent', () => {
         "export { default as <%= name %> } from './<%= name %>';\n",
         { name : 'Cor_' + componentName}
     );
-    
 });
 
 /*** HELPERS ***/
@@ -71,6 +52,18 @@ function addFooter(src, dest, footerTemplate, footerParams) {
     gulp.src(src)
         .pipe(footer(footerTemplate, footerParams ))
         .pipe(gulp.dest(dest));
+}
+
+function validateName (componentName, expectedParam, checkFirstIsUppercase) {
+    if(!componentName) {
+        console.error("ERR! You are not select mandatory parameter value " + expectedName);
+        return false;
+    }
+
+    if(componentName[0] !== componentName[0].toUpperCase() && checkFirstIsUppercase) {
+        console.error("ERR!"+ expectedName + " value must start with uppercase");
+        return false;
+    }
 }
 
 function getArg(name) {
