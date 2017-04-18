d19e779a3601fe49a11907961d7297d2d5136c7f
refaelok <refaeloknin@gmail.com>
2017-04-19 01:22:18 +0300

    Upgrade Requests calls and usage

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,18 +3,4 @@
  */
 
 
-const REDUXBLOG_ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-
-const API_KEY = 'refaelok';
-const POSTS_URL = `${REDUXBLOG_ROOT_URL}/posts`;
-
-
-export default api = {
-
-    fetchPosts: {
-        method: 'get',
-        url: POSTS_URL,
-        params: {key: API_KEY}
-    },
-
-}
+export const REDUXBLOG_ROOT_URL = 'http://reduxblog.herokuapp.com/api';
